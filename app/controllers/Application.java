5fc26e314123a235a9095c73cfabcc266ea59637
John DeFour <jpdefour@oakland.edu>
2014-03-18 18:24:25 -0400

    Added the navbar planets and the other stuff.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -19,16 +19,38 @@ public class Application extends Controller {
         return ok(contact.render());
     }
 
-    public static Result saturn() {
-        return ok(saturn.render());
+    public static Result mercury() {
+        return ok(mercury.render());
+    }
+
+    public static Result venus() {
+        return ok(venus.render());
+    }
+
+    public static Result earth() {
+        return ok(earth.render());
+    }
+
+    public static Result mars() {
+        return ok(mars.render());
     }
 
     public static Result jupiter() {
         return ok(jupiter.render());
     }
 
-    public static Result mercury() {
-        return ok(mercury.render());
+    public static Result saturn() {
+        return ok(saturn.render());
     }
+
+    public static Result neptune() {
+        return ok(neptune.render());
+    }
+
+    public static Result uranus() {
+        return ok(uranus.render());
+    }
+
+
 
 }
