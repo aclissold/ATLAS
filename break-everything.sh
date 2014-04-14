8da11e9278dca23433d188b90dc55c69355dd340
Andrew Clissold <andrewclissold@gmail.com>
2014-04-14 14:41:33 -0400

    Lower the number of script for-loop iterations

--- break-everything.sh
+++ break-everything.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for i in {1..300}
+for i in {1..100}
 do
     curl "localhost:9000/comment/delete/$i"
 done
 
-for i in {1..300}
+for i in {1..100}
 do
     curl --data "page=Saturn&user=Monty+Python&text=spamspamspamspamspam" \
         localhost:9000/comment/save
